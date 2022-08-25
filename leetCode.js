ce910f4492d030577ad6da48cad9badcb908ba90
Andrey <spacecmetanka@gmail.com>
2022-08-25 16:55:40 +0300

    746

--- leetCode.js
+++ leetCode.js
@@ -389,17 +389,3 @@
     console.log(maxScore(cardPoints, k))
 }
 
-{
-    // 746 . Минимальная стоимость подъема по лестнице
-    const price = [1, 100, 2, 3, 3, 103, 4, 5, 104, 6]
-    const minCostClimbingStairs = (cost) => {
-        for (let i = 2; i < cost.length; i++) {
-            cost[i] = cost[i] + Math.min(cost[i - 2], cost[i - 1]);
-            console.log(cost[i])
-        }
-        // console.log(cost)
-        return Math.min(cost[cost.length - 2], cost[cost.length - 1]);
-    }
-
-    console.log(minCostClimbingStairs(price))
-}
